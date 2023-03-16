c36045dc544908147bca4201e552e6f7fd9612cf
Gabriel Huet de Froberville <gabriel.huet-de-froberville@epita.fr>
2023-03-16 15:44:17 +0100

    feat(neovim): Change config and use nvim-config from github

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,42 +1,17 @@
 { pkgs }:
 let
-  plugins = with pkgs.vimPlugins; [
-    vim-nix
-    vim-airline
-  ];
-  extraConfig = ''
-          set termguicolors
-
-          " tab help
-          set expandtab
-          set smarttab
-          set shiftwidth=2
-          " -1 means the same value as shiftwidth
-          set softtabstop=-1
-
-          " Set the minimal amount of lignes under and above the cursor
-          " Useful for keeping context when moving with j/k
-          set scrolloff=8
-
-          set ai " Auto indent
-          set si " Smart indent
-          set wrap " Wrap lines
-          set swapfile!
-
-          set relativenumber
-          set number
-
-          set colorcolumn=80
-
-          " Apply automatically formatter
-
-    autocmd BufWritePost *.nix silent !nixpkgs-fmt <afile>
-    autocmd BufWritePost *.fish silent !fish_indent <afile>
-    autocmd BufWritePost *.py silent !black <afile>
-  '';
+  neovim-config = builtins.fetchGit {
+    url = "https://github.com/TimotheeDesveaux/nvim-config";
+    ref = "refs/heads/main";
+    rev = "c8fe64a919b7450a44f00e0dabb21adbf83b1156";
+  };
 in
 {
   enable = true;
   vimAlias = true;
-  inherit extraConfig plugins;
+  extraLuaConfig = ''
+    package.path = package.path ..
+        ";${neovim-config}/lua/?/init.lua;${neovim-config}/lua/?.lua"
+    dofile("${neovim-config}/init.lua")
+  '';
 }
