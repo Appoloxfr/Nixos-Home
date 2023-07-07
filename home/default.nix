a18cf3f65be377275158ccb258c954554490bd40
Gabriel Huet de Froberville <gabriel.huet-de-froberville@epita.fr>
2023-07-07 18:54:31 +0200

    feat(home: files): Create dot files

--- home/default.nix
+++ home/default.nix
@@ -4,12 +4,22 @@ let
 in
 rec {
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      waybar = prev.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+        postPatch = (oldAttrs.postPatch or "") + ''
+          sed -i 's/zext_workspace_handle_v1_activate(workspace_handle_);/const std::string command = "hyprctl dispatch workspace " + name_;\n\tsystem(command.c_str());/g' src/modules/wlr/workspace_manager.cpp'';
+      });
+    })
+  ];
 
   home = {
     inherit homeDirectory stateVersion username;
     packages = import ./packages {
       inherit pkgs;
     };
+    file = import ./files { inherit pkgs; };
   };
 
   # Configuration of programs
@@ -30,7 +40,8 @@ rec {
   fonts.fontconfig.enable = true;
 
   # xsession = import ./xsession.nix { inherit pkgs lib; };
-  wayland = import ./wayland.nix;
+  wayland = import ./wayland.nix { inherit pkgs; };
 
-  services = import ./services;
+  services = import ./services { inherit pkgs; };
+  systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
 }
