4521db4269f16eb6949f39f24a85262fbf4f77a5
Gabriel Huet de Froberville <gabriel.huet-de-froberville@epita.fr>
2023-07-16 18:46:56 +0200

    feat(swayidle): Add new type of idle

--- home/files/default.nix
+++ home/files/default.nix
@@ -1,5 +1,9 @@
 { pkgs }:
+let
+  config_swayidle = import ./swayidle/default.nix { inherit pkgs; };
+in
 {
   ".config/swaylock/config".source = ./swaylock/config;
-  ".config/swayidle/config".source = ./swayidle/config;
+  ".config/swayidle/config".text = config_swayidle.default;
+  ".config/swayidle/music".text = config_swayidle.music;
 }
